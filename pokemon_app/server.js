a3ca979061c0f333065fb70c774787b243368e32
Jose Salazar <jsalazar@pneumarespiratory.com>
2024-01-15 10:30:00 -0500

    index.jsx view rendered at pokemon route

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -5,12 +5,19 @@ const app = express()
 
 const PORT = 3000
 
+//load our engine
+const jsxEngine = require('jsx-view-engine')
+
+// setup our engine
+app.set('view engine', 'jsx')
+app.engine('jsx',jsxEngine())
+
 app.get('/',(req,res) => {
     res.send('Welcome to the Pokemon App!')
 })
 
 app.get('/pokemon', (req,res) =>{
-    res.send(pokemon)
+    res.render('Index', pokemon)
 })
 
 app.listen(PORT, () =>{
